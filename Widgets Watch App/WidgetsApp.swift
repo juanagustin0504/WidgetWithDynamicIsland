3b819dd1c6f6d38d6688610569c939ffafee4696
moon-john <moon-john@naver.com>
2022-12-13 09:10:07 +0900

    Initial Commit

---
+++ Widgets Watch App/WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsApp.swift
+//  Widgets Watch App
+//
+//  Created by Webcash on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct Widgets_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Widgets/WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsApp.swift
+//  Widgets
+//
+//  Created by Webcash on 2022/12/13.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
